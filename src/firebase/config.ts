56308a4f71367ef559d358866fe97693bd45f7cd
Sol Carnascari <solcarnascari@gmail.com>
2025-11-13 20:44:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1349053739-a3a5d",
+  "appId": "1:212522418:web:bed58082a871be7c8700ab",
+  "apiKey": "AIzaSyC4h3fL3peAoFnNaxoUb8fwhIwWl-lgFIk",
+  "authDomain": "studio-1349053739-a3a5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "212522418"
+};
